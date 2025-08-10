f6a3a064bfb99e697eee2b70241220d29a3d040d
amirrezash0n <shourvarziamirreza@gmail.com>
2025-08-10 22:39:20 +0330

    Create pages components & Initial tailwind Css

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2021: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "warn",
+    "react/jsx-uses-react": "error",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
